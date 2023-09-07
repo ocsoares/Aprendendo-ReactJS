e5a7b1f8b8015781db1a715d43bab8e2981ca23d
ocsoares <casoares016@gmail.com>
2023-09-07 12:37:46 -0300

    feat: add children inside to Header component

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -1,4 +1,3 @@
-import { PropsWithChildren } from "react";
 import { Children } from "./Children";
 
 interface IHeaderProps {
@@ -6,14 +5,13 @@ interface IHeaderProps {
   subtitle: string;
 }
 
-export function Header(props: PropsWithChildren<IHeaderProps>) {
-  const { title, subtitle, children } = props;
+export function Header(props: IHeaderProps) {
+  const { title, subtitle } = props;
 
   return (
     <div>
       <h1 style={{ color: "darkcyan" }}>{title}</h1>
       <span style={{ color: "firebrick" }}>{subtitle}</span>
-      <p style={{ color: "darkorchid" }}>{children}</p>
       <Children />
       <Children />
       <Children />
