ab7297ec6fd2a4023a20b0bee0c0b52e14b887cd
ocsoares <casoares016@gmail.com>
2023-09-24 19:12:35 -0300

    feat: add loading buttons props to Toolbar

--- src/app/shared/components/Toolbar/Toolbar.tsx
+++ src/app/shared/components/Toolbar/Toolbar.tsx
@@ -1,5 +1,5 @@
 import { Add, ArrowBack, Delete, Save } from "@mui/icons-material";
-import { Box, Divider, Paper, useTheme } from "@mui/material";
+import { Box, Divider, Paper, Skeleton, useTheme } from "@mui/material";
 import { ToolButton } from "../ToolButton";
 
 interface IToolbarProps {
@@ -14,6 +14,12 @@ interface IToolbarProps {
   showDeleteButton?: boolean;
   showNewButton?: boolean;
   showBackButton?: boolean;
+
+  showSaveButtonLoading?: boolean;
+  showSaveAndReturnButtonLoading?: boolean;
+  showDeleteButtonLoading?: boolean;
+  showNewButtonLoading?: boolean;
+  showBackButtonLoading?: boolean;
 
   onClickSaveButton?: () => void;
   onClickSaveAndReturnButton?: () => void;
@@ -35,6 +41,12 @@ export const Toolbar = ({
   showNewButton = true,
   showBackButton = true,
 
+  showSaveButtonLoading = false,
+  showSaveAndReturnButtonLoading = false,
+  showDeleteButtonLoading = false,
+  showNewButtonLoading = false,
+  showBackButtonLoading = false,
+
   onClickSaveButton,
   onClickSaveAndReturnButton,
   onClickDeleteButton,
@@ -54,7 +66,7 @@ export const Toolbar = ({
       display={"flex"}
       alignItems={"center"}
     >
-      {showSaveButton && (
+      {showSaveButton && !showSaveButtonLoading && (
         <ToolButton
           startIcon={<Save />}
           text={textSaveButton}
@@ -62,7 +74,10 @@ export const Toolbar = ({
         />
       )}
 
-      {showSaveAndReturnButton && (
+      {/* Isso vai no LUGAR do BOTÃO quando ele estiver, por algum Motivo, CARREGANDO !!! */}
+      {showSaveButtonLoading && <Skeleton width={91.83} height={61} />}
+
+      {showSaveAndReturnButton && !showSaveAndReturnButtonLoading && (
         <ToolButton
           startIcon={<Save />}
           text={textSaveAndReturnButton}
@@ -72,7 +87,11 @@ export const Toolbar = ({
         />
       )}
 
-      {showDeleteButton && (
+      {showSaveAndReturnButtonLoading && (
+        <Skeleton width={184.29} height={60.2} />
+      )}
+
+      {showDeleteButton && !showDeleteButtonLoading && (
         <ToolButton
           startIcon={<Delete />}
           text={textDeleteButton}
@@ -82,7 +101,9 @@ export const Toolbar = ({
         />
       )}
 
-      {showNewButton && (
+      {showDeleteButtonLoading && <Skeleton width={107.12} height={60.2} />}
+
+      {showNewButton && !showNewButtonLoading && (
         <ToolButton
           startIcon={<Add />}
           text={textNewButton}
@@ -92,9 +113,11 @@ export const Toolbar = ({
         />
       )}
 
+      {showNewButtonLoading && <Skeleton width={86.98} height={60.2} />}
+
       <Divider variant="middle" orientation="vertical" sx={{ height: "65%" }} />
 
-      {showBackButton && (
+      {showBackButton && !showBackButtonLoading && (
         <ToolButton
           startIcon={<ArrowBack />}
           text={textBackButton}
@@ -103,6 +126,8 @@ export const Toolbar = ({
           color="secondary"
         />
       )}
+
+      {showBackButtonLoading && <Skeleton width={93.18} height={60.2} />}
     </Box>
   );
 };
